c23a7c5ad3cd3a828129138bdc27ed2d7bb9103a
周恩波 <zhouenbo@lx-dtx.com>
2024-08-29 15:27:37 +0800

    feat: 添加husky

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: 周恩波 zhouenbo@lx-dtx.com
  * @Date: 2024-08-29 14:07:07
  * @LastEditors: 周恩波
- * @LastEditTime: 2024-08-29 14:23:55
+ * @LastEditTime: 2024-08-29 15:14:24
  * @Description:
  */
 import { createApp } from 'vue'
